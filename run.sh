35e54ae883c8f4c30839f5d9a4caff79c079bef3
yqq <youngqqcn@gmail.com>
2022-02-28 21:52:06 +0800

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 nohup python monitor.py > /var/log/monitor.log  2>&1 &
 nohup python server.py > /var/log/server.log  2>&1 &
+
+# just keep this script running
+while [[ true ]]; do
+    sleep 1
+done

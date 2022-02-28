c01b6de958ec9fd4c7d04e4e4f170342e963740f
yqq <youngqqcn@gmail.com>
2022-02-28 16:56:08 +0800

    fix

--- monitor.py
+++ monitor.py
@@ -70,7 +70,7 @@ def loop():
                     if k == 'bitcoin':
                         btc_offline_workers.append(w_name)
 
-    if len(eth_offline_workers) == 0 and len(btc_offline_workers) == 0 and len(ltc_offline_workers) > 0:
+    if len(eth_offline_workers) == 0 and len(btc_offline_workers) == 0 and len(ltc_offline_workers) == 0:
         logging.info("没有机子掉线")
         return
     
